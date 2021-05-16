9bf591ce9c33eccf7ed57ad381bba7d021bbf887
Max Zabarka <maxim.zabarka@gmail.com>
2021-05-15 23:24:25 -0600

    Scripted auto-commit on change (2021-05-15 23:24:25) by gitwatch.sh

--- src/components/Home/ScrollButton/ScrollButton.js
+++ src/components/Home/ScrollButton/ScrollButton.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import arrow from "../../../assets/down-chevron.svg"
-
+import "./ScrollButton"
 const ScrollButton = () => {
     return (
         <div className="ScrollButton">
