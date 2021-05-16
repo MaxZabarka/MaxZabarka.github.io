d54f8dc64e0a36443a509a128c17f58a8ecbfcdb
Max Zabarka <maxim.zabarka@gmail.com>
2021-05-15 23:05:08 -0600

    Scripted auto-commit on change (2021-05-15 23:05:08) by gitwatch.sh

--- src/components/Shared/Background/Background.js
+++ src/components/Shared/Background/Background.js
@@ -2,7 +2,9 @@ import React from 'react';
 import "./Background.scss"
 
 const Background = (props) => {
-    
+    const style = {
+        back
+    }
     return (
         <div style={style} className={props.className + " Background"}/>
     );
