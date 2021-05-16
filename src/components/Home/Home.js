29ca1dfc15e3a5862839dc3011722339b359eb1b
Max Zabarka <maxim.zabarka@gmail.com>
2021-05-15 23:20:59 -0600

    Scripted auto-commit on change (2021-05-15 23:20:59) by gitwatch.sh

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Title from './Title/Title';
 import "./Home.scss"
+import ScrollButton from './ScrollButton/ScrollButton';
 
 const Home = () => {
     return (
         <div className="Home">
             <Title/>
+            <ScrollButton/>
         </div>
     );
 };
