9622872c928c730cfe5b70ebe626368cc6c13cf4
Max Zabarka <maxim.zabarka@gmail.com>
2021-05-15 23:13:04 -0600

    Scripted auto-commit on change (2021-05-15 23:13:04) by gitwatch.sh

--- src/components/Home/Title/Title.js
+++ src/components/Home/Title/Title.js
@@ -4,7 +4,7 @@ import "./Title.scss"
 const Title = () => {
     return (
         <div className="Title">
-            <h1>Hey, my name is Max</h1>
+            <h1>Hey, I am Max</h1>
             <h2>Let's turn your <i>ideas</i> into <i>reality</i></h2>
         </div>
     );
