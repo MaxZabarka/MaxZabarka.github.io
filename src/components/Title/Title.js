ee1c9f86fc8b2f21e7f484ad18c865bed7b3d0d8
Max Zabarka <maxim.zabarka@gmail.com>
2021-05-15 22:46:20 -0600

    Scripted auto-commit on change (2021-05-15 22:46:20) by gitwatch.sh

---
+++ src/components/Title/Title.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./Title.scss"
+const Title = () => {
+    return (
+        <div className="Title.scss">
+            
+        </div>
+    );
+};
+
+export default Title;
