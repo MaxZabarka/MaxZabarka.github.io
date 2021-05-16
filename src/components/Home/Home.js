05412681f16d3acefe9d5d49935c20f9333a24cc
Max Zabarka <maxim.zabarka@gmail.com>
2021-05-15 23:37:08 -0600

    Scripted auto-commit on change (2021-05-15 23:37:08) by gitwatch.sh

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,11 @@
-import React, { useEffect } from 'react';
-import Title from './Title/Title';
-import "./Home.scss"
-import ScrollButton from './ScrollButton/ScrollButton';
+import React, { useEffect } from "react";
+import Title from "./Title/Title";
+import "./Home.scss";
+import ScrollButton from "./ScrollButton/ScrollButton";
 
-function resetHeight(){
-    // reset the body height to that of the inner browser
-    document.body.style.height = window.innerHeight + "px";
+function resetHeight() {
+  // reset the body height to that of the inner browser
+  document.body.style.height = window.innerHeight + "px";
 }
 
 window.addEventListener("resize", resetHeight);
@@ -13,24 +13,24 @@ window.addEventListener("resize", resetHeight);
 resetHeight();
 
 const Home = () => {
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll);
-      });
-      useEffect(() => {
-        return () => {
-          window.removeEventListener('scroll', handleScroll);
-        };
-      }, []);
-    
-      function handleScroll() {
-        let scrollTop = window.scrollY;
-      }
-    return (
-        <div className="Home">
-            <Title/>
-            <ScrollButton/>
-        </div>
-    );
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  });
+  useEffect(() => {
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  function handleScroll() {
+    let scrollTop = window.scrollY;
+  }
+  return (
+    <div className="Home">
+      <Title />
+      <ScrollButton />
+    </div>
+  );
 };
 
 export default Home;
