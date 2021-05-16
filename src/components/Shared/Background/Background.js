4807e281b436eccf6b9f60067ea1685a56d559e8
Max Zabarka <maxim.zabarka@gmail.com>
2021-05-15 23:06:06 -0600

    Scripted auto-commit on change (2021-05-15 23:06:06) by gitwatch.sh

--- src/components/Shared/Background/Background.js
+++ src/components/Shared/Background/Background.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import "./Background.scss"
+import React from "react";
+import "./Background.scss";
 
 const Background = (props) => {
-    const style = {
-        backgroundImage: props.image
-    }
-    return (
-        <div styles={{ backgroundImage:`url(${props.image})` }} className={props.className + " Background"}/>
-    );
+  return (
+    <div
+      styles={{ backgroundImage: `url(${props.image})` }}
+      className={props.className + " Background"}
+    />
+  );
 };
 
 export default Background;
