cd88f0ee69517c63daf7d4afe928bda75c35220b
Max Zabarka <maxim.zabarka@gmail.com>
2021-05-15 23:37:02 -0600

    Scripted auto-commit on change (2021-05-15 23:37:02) by gitwatch.sh

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Title from './Title/Title';
 import "./Home.scss"
 import ScrollButton from './ScrollButton/ScrollButton';
