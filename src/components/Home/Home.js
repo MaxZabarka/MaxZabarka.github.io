56b95446a8aa58b05037dfca98ff250d43a0c27c
Max Zabarka <maxim.zabarka@gmail.com>
2021-05-15 23:39:32 -0600

    Scripted auto-commit on change (2021-05-15 23:39:32) by gitwatch.sh

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,12 +3,8 @@ import Title from "./Title/Title";
 import "./Home.scss";
 import ScrollButton from "./ScrollButton/ScrollButton";
 
-
-
-
-
 const Home = () => {
-    const homeElement = useRef(null);
+  const homeElement = useRef(null);
 
   useEffect(() => {
     window.addEventListener("resize", resetHeight);
@@ -21,7 +17,7 @@ const Home = () => {
   }, []);
 
   function resetHeight() {
-      console.log('window.innerHeight :>> ', window.innerHeight);
+    console.log("window.innerHeight :>> ", window.innerHeight);
     // reset the body height to that of the inner browser
     // document.body.style.height = window.innerHeight + "px";
   }
