e21dc35f202c1d316598378400a78cd921d848a1
Max Zabarka <maxim.zabarka@gmail.com>
2021-05-15 23:36:02 -0600

    Scripted auto-commit on change (2021-05-15 23:36:02) by gitwatch.sh

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,6 +3,15 @@ import Title from './Title/Title';
 import "./Home.scss"
 import ScrollButton from './ScrollButton/ScrollButton';
 
+function resetHeight(){
+    // reset the body height to that of the inner browser
+    document.body.style.height = window.innerHeight + "px";
+}
+// reset the height whenever the window's resized
+window.addEventListener("resize", resetHeight);
+// called to initially set the height.
+resetHeight();
+
 const Home = () => {
     return (
         <div className="Home">
