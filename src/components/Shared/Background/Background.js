0ba69e4bc440bc1863e8886ab7159a701a5157c4
Max Zabarka <maxim.zabarka@gmail.com>
2021-05-15 23:04:16 -0600

    Scripted auto-commit on change (2021-05-15 23:04:16) by gitwatch.sh

---
+++ src/components/Shared/Background/Background.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Background.scss"
+
+const Background = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Background;
